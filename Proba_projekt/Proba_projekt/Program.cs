f1d9038f6d20c642201063b239b2f91de2f93734
Ledaacs <ledacsbence@gmail.com>
2024-01-19 09:29:50 +0100

    Projekt létrehozása

---
+++ Proba_projekt/Proba_projekt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.ReadAllText("Valami.txt"));
+
+            Console.ReadLine();
+        }
+    }
+}
